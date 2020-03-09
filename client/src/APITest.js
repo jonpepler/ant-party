8e2e6667d51906591de96168da628f8329428859
Jon Pepler <jonathan.pepler@gmail.com>
2020-03-09 00:36:19 +0000

    feat: 🎸 Show connection error in footer when con fails happen

--- client/src/APITest.js
+++ client/src/APITest.js
@@ -7,6 +7,7 @@ const APITest = () => {
     fetch(`${process.env.REACT_APP_SERVER_HOST}/api/v1/test`)
       .then(res => res.text())
       .then(res => setAPIResponse(res))
+      .catch(error => setAPIResponse(`${process.env.REACT_APP_SERVER_HOST}: ${error.message}`))
   }
   if (apiResponse === '') callAPI()
 
