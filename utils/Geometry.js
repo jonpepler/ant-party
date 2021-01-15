e568b53f6143d2e1994d757dd2fe7da2a2b2f8fb
Jon Pepler <jonathan.pepler@gmail.com>
2021-01-15 15:55:59 +0000

    refactor: 💡 Drop point-in-polygon package for simpler impl
    Now requires shapes to be squares to be accurate. The advantage is that
    it supports points that are on the edge of a polygon as well as fully
    inside them.

--- utils/Geometry.js
+++ utils/Geometry.js
@@ -1,11 +1,16 @@
-const inside = require('point-in-polygon')
-
 module.exports = {
   Coord: (x, y) => ({ x, y }),
 
   translateShape: (shape, dx, dy) => shape.map(coord => ({ x: coord.x + dx, y: coord.y + dy })),
 
   pointInPolygon: (pointX, pointY, shapePoints) => {
-    return inside([pointX, pointY], shapePoints)
+    const minX = Math.min(...shapePoints.map(sp => sp[0]))
+    const minY = Math.min(...shapePoints.map(sp => sp[1]))
+    const maxX = Math.max(...shapePoints.map(sp => sp[0]))
+    const maxY = Math.max(...shapePoints.map(sp => sp[1]))
+    return pointX >= minX &&
+          pointX <= maxX &&
+          pointY >= minY &&
+          pointY <= maxY
   }
 }
